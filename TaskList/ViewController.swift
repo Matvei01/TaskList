9f6f62ab868585217738ee61375f08e6b9fdd672
Matvei95 <khlestov.95@mail.ru>
2022-12-02 23:25:37 +0500

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by Matvei Khlestov on 02.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
